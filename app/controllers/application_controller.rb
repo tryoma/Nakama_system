fc5189c0ad2659829674fa46d8a95fa1c943370c
tryoma <tryoma0326@gmail.com>
2020-11-15 11:03:41 +0900

    add_username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # サインイン後のリダイレクト先をマイページへ
   def after_sign_in_path_for(resource)
     flash[:notice] = "ログインに成功しました" #　 <-任意で
@@ -9,5 +12,14 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     flash[:alert] = "ログアウトしました"
     root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :email, :username, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_15_005049) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "username", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
