fcc6a67ef34fc0f8710ebecc4159146c8911dda8
tryoma <tryoma0326@gmail.com>
2020-11-15 20:49:15 +0900

    create_user_index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#home'
   get '/top', to: 'home#top'
+  
+  resources :users, only: [:index]
   namespace :api, format: 'json' do
     resources :users, only: [:index, :create, :destroy, :update]
   end
