a424c6ce5e3b8eee059b77e662833fb771477e96
tryoma <tryoma0326@gmail.com>
2020-11-17 21:19:33 +0900

    create_group_new_page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,7 +12,14 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @group
+    else
+      render :new
+    end
+
   end
 
   def edit
@@ -20,4 +27,10 @@ class GroupsController < ApplicationController
 
   def update
   end
+
+  private
+
+    def group_params
+      params.require(:group).permit(:group_name, :email, :goal, :todo, :frequency, :start_goal_day, :end_goal_day, :genre, :limit_members, :owner_id )
+    end
 end
